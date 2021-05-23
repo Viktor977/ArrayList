471fa7bf645cca56b770899b84e20545843e3ea1
Viktor Stepanov <angelinavirta@gmail.com>
2021-05-23 21:46:31 +0300

    Lessons(System collections)

---
+++ ArrayList/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ArrayList
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var list = new System.Collections.ArrayList();
+            //добавление одиночных элементов
+            string s = "Hello";
+            list.Add(s);
+            list.Add("hi");
+            list.Add(50);
+            list.Add(new object());
+           
+            //добавление групп элементов
+            var anArray = new[] {"more", "or", "and"};
+            list.AddRange(anArray);
+            var anatheArrey = new[] {new object(), new System.Collections.ArrayList()};
+            list.Add(anatheArrey);
+        }
+    }
+}
